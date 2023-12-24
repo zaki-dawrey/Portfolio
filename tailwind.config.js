d92a2eef94067d3c7548e3b30f9f72cdadd7c733
Zaki Dawrey <zdawrey@gmail.com>
2023-12-24 19:30:46 +0530

    Changes in navbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,11 @@ module.exports = {
       lg: '976px',
       xl: '1440px',
     },
-    extend: {},
+    extend: {
+      fontFamily: {
+        'poppins': ['Poppins', 'sans-serif']
+      },
+    },
   },
   plugins: [],
 }
